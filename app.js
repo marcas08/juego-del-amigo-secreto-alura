4319c240ced6d061c7a1177f6cefef83776709d7
marcas08 <marcas314@gmail.com>
2025-03-06 22:38:19 -0600

    Se agregaron las siguientes funciones:  contieneNumero ()  Revisa que el dato ingresado por el usuario no contenga numeros ni caracteres especiales.
     agregarNombre ()
     Agrega el nombre valido al arreglo para su posterior sorteo.
    
     validarNombre ()
    Revisa que no se trate de ingresar un campo vacio, ademas de llamar a la funcion
    contieneNumero () y agregarNombre () Segun sea necesario.

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+let minuscula = ""
+
+function contieneNumero () {
+    let prueba = document.getElementById ('amigo').value
+    var especial = /[^A-Za-z 0-9]/g;
+    return especial.test (prueba)
+    
+}
+
+function agregarNombre (){
+    amigos.push (minuscula);
+    document.getElementById ('amigo').value = '';
+    }
+
+function validarElNombre () {
+    minuscula = document.getElementById ('amigo').value;
+    minuscula = minuscula.toLowerCase()
+    contieneNumero ()
+    if (document.getElementById ('amigo').value == ''){ 
+        alert ("Por favor, inserte un nombre.");
+   
+    }else if (contieneNumero () == true)  {
+        alert ("Por favor, ingrese solo letras.");
+        document.getElementById ('amigo').value = '';
+    
+    }else if  (amigos.includes (minuscula)){
+        alert ("Ingrese un nombre diferente.");
+            document.getElementById ('amigo').value = '';
+        
+    }else {
+
+        agregarNombre();
+        console.log (amigos);
+    }
+
+}
